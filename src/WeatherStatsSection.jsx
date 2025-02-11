15f9dd533ca1d7aaa516b723ff45515715ca415a
Kacper Grabowski <kacperg390@gmail.com>
2025-02-11 18:10:37 +0000

    Added props to WeatherStatsSection.jsx

--- src/WeatherStatsSection.jsx
+++ src/WeatherStatsSection.jsx
@@ -1,7 +1,7 @@
 import StatsCard from "./Card/StatsCard.jsx"
 import DaysCard from "./Card/DaysCard.jsx"
 
-function WeatherStatsSection() {
+function WeatherStatsSection(props) {
     return (
         <>
             <div className="days-title-container flex flex-center">
@@ -9,16 +9,16 @@ function WeatherStatsSection() {
             </div>
 
             <div className="weather-days-row flex flex-center">
-                <DaysCard></DaysCard>
+                <DaysCard weatherCodes={props.weatherCodes} maxTemperatures={props.maxTemperatures} minTemperatures={props.minTemperatures}></DaysCard>
             </div>
 
             <div className="weather-card-row flex flex-center">
-                <StatsCard title="Wind"></StatsCard>
-                <StatsCard title="Humidity"></StatsCard>
+                <StatsCard title="Wind" value={props.windSpeed} unit="km/h"></StatsCard>
+                <StatsCard title="Humidity" value={props.humidity} unit="%"></StatsCard>
                 <StatsCard title="Pressure"></StatsCard>
             </div>
         </>
-    )
+    );
 }
 
 export default WeatherStatsSection

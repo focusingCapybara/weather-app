a26e90a99ea4c82442b133428706c02927384822
Kacper Grabowski <kacperg390@gmail.com>
2025-02-13 13:37:32 +0000

    Input box value is cleared when entered

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,7 @@ function Header(props) {
             if (event.key === 'Enter') {
                 event.preventDefault();
                 const data = searchBtn.value;
+                searchBtn.value = "";
                 props.getFromHeader(data);
             }
         };

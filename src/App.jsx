505f7bb17ec8fb56b78d02067182bd7b14625739
Kacper Grabowski <kacperg390@gmail.com>
2025-02-17 19:31:43 +0000

    Optimized code and added comments

--- src/App.jsx
+++ src/App.jsx
@@ -9,73 +9,32 @@ function App() {
 	const [error, setError] = useState(null);
 	const [weatherData, setWeatherData] = useState(null);
 	const [geoData, setGeoData] = useState(null);
+	const [place, setPlace] = useState(null);
 	const [locationName, setLocationName] = useState(null);
 
-	async function fetchGeoLocation(place) {
-		try {
-			const GEO_API = import.meta.env.VITE_GEO_API_KEY;
-			const URL = `https://api.opencagedata.com/geocode/v1/json?q=${place}&key=${GEO_API}`;
-			const response = await fetch(URL);
-
-			if (!response.ok) {
-				throw new Error("Network response was not ok");
-			}
-
-			const geoData = await response.json();
-
-			console.log(geoData);
-
-			setGeoData(geoData);
-
-			let locationName = geoData.results[0].formatted;
-
-			for (let i = 0; i < locationName.length; i++) {
-				if (locationName[i] === ",") {
-					locationName = locationName.slice(0, i);
-				}
-			}
-
-			setLocationName(locationName);
-
-			fetchWeatherData(geoData);
-		}
-		catch (err){
-			setError(err.message)
-		}
-	}
-
-	async function fetchWeatherData(geoData) {
-		try {
-			const WEATHER_URL = `https://api.open-meteo.com/v1/forecast?latitude=${geoData.results[0].geometry.lat}&longitude=${geoData.results[0].geometry.lng}&current=temperature_2m,relative_humidity_2m,is_day,rain,weather_code,surface_pressure,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum&timezone=Europe%2FLondon`;
-			const response = await fetch(WEATHER_URL);
-
-			if (!response.ok) {
-				throw new Error("Network response was not ok");
-			}
-
-			const weatherData = await response.json();
-
-			console.log(weatherData);
-
-			setWeatherData(weatherData);
-		}
-		catch (err) {
-			setError(err.message);
-		}
-	};
-
+	// Gets the current units
 	function getUnitsFromHero(isMetric) {
 		setIsMetric(isMetric);
 	}
 
-	function getFromHero(data) {
-		fetchGeoLocation(data);
+	// Gets place name
+	function getPlaceNameFromHero(data) {
+		setPlace(data);
 	}
 
 	useEffect(() => {
-		async function fetchWeatherData() {
+		async function fetchWeatherData(geoData) {
+			let WEATHER_URL;
+
+			if (geoData == null) {
+				WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=55.9521&longitude=-3.1965&current=temperature_2m,relative_humidity_2m,is_day,rain,weather_code,surface_pressure,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum&timezone=Europe%2FLondon";
+
+			}
+			else {
+				WEATHER_URL = `https://api.open-meteo.com/v1/forecast?latitude=${geoData.results[0].geometry.lat}&longitude=${geoData.results[0].geometry.lng}&current=temperature_2m,relative_humidity_2m,is_day,rain,weather_code,surface_pressure,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum&timezone=Europe%2FLondon`;
+			}
+
 			try {
-				const WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=55.9521&longitude=-3.1965&current=temperature_2m,relative_humidity_2m,is_day,rain,weather_code,surface_pressure,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,rain_sum,showers_sum&timezone=Europe%2FLondon";
 				const response = await fetch(WEATHER_URL);
 
 				if (!response.ok) {
@@ -93,10 +52,18 @@ function App() {
 			}
 		};
 
-		async function fetchGeoLocation() {
+		async function fetchGeoLocation(place) {
+			const GEO_API = import.meta.env.VITE_GEO_API_KEY;
+			let URL;
+
+			if (place == null) {
+				URL = `https://api.opencagedata.com/geocode/v1/json?q=Edinburgh&key=${GEO_API}`;
+			}
+			else {
+				URL = `https://api.opencagedata.com/geocode/v1/json?q=${place}&key=${GEO_API}`;
+			}
+
 			try {
-				const GEO_API = import.meta.env.VITE_GEO_API_KEY;
-				const URL = `https://api.opencagedata.com/geocode/v1/json?q=Dalkeith+1%EH222JR&key=${GEO_API}`;
 				const response = await fetch(URL);
 
 				if (!response.ok) {
@@ -124,11 +91,12 @@ function App() {
 			}
 		}
 		
-		fetchWeatherData();
-		fetchGeoLocation();
-	}, []);
+		fetchWeatherData(geoData);
+		fetchGeoLocation(place);
+	}, [place]);
 
 
+	// What happens when data is not loaded
 	if (!weatherData) {
 		document.body.style.opacity = 1;
 		return (
@@ -136,13 +104,14 @@ function App() {
 		);
 	}
 
+	// Display error if something went wrong with getting data
 	if (error) {
 		return <div>Error</div>
 	}
 
 	return (
 		<>
-			<Hero getUnitsFromHero={getUnitsFromHero} getFromHero={getFromHero} isMetric={isMetric} locationName={locationName} date={weatherData.daily.time[0]} weatherCode={weatherData.current.weather_code} temperature={weatherData.current.temperature_2m}></Hero>
+			<Hero getUnitsFromHero={getUnitsFromHero} getPlaceNameFromHero={getPlaceNameFromHero} isMetric={isMetric} locationName={locationName} date={weatherData.daily.time[0]} weatherCode={weatherData.current.weather_code} temperature={weatherData.current.temperature_2m}></Hero>
 			<HourForecast isMetric={isMetric} temperatures={weatherData.hourly.temperature_2m} weatherCodes={weatherData.hourly.weather_code}></HourForecast>
 			<WeatherStatsSection isMetric={isMetric} windSpeed={weatherData.current.wind_speed_10m} humidity={weatherData.current.relative_humidity_2m} pressure={weatherData.current.surface_pressure} maxTemperatures={weatherData.daily.temperature_2m_max} minTemperatures={weatherData.daily.temperature_2m_min} weatherCodes={weatherData.daily.weather_code} dailyDates={weatherData.daily.time}></WeatherStatsSection>
 		</>

88f3ded27ed922aab56e20337902d79104daff4c
Kacper Grabowski <kacperg390@gmail.com>
2025-03-03 20:30:47 +0000

    Added class icon for current section

--- src/components/Card/StatsCard.jsx
+++ src/components/Card/StatsCard.jsx
@@ -4,6 +4,7 @@ function Card(props) {
     return (
         <div className={`${styles.statsCard} ${styles.card} flex flex-col flex-center`}>
             <h3>{props.title}</h3>
+            <div className="stats-icon">{props.icon}</div>
             <span className={styles.valueItem}>{props.value}{props.unit}</span>
         </div>
     );

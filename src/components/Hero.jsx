87fbdab3aa0ea32d13f1e68a677da0c9d1236457
Kacper Grabowski <kacperg390@gmail.com>
2025-03-11 17:25:05 +0000

    Optimized theme changer code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,31 +45,35 @@ function Hero(props) {
         return weatherIcons[props.weatherCode] || "Unknown Icon";
     }
 
+    function changeTheme(background1, background2, font="#000000") {
+        document.documentElement.style.setProperty("--dynamic-background-1", background1);
+        document.documentElement.style.setProperty("--dynamic-background-2", background2);
+        document.documentElement.style.setProperty("--hero-font-colour", font);
+    }
+
     const icon = getWeatherIcon();
 
-    if (icon.type.name === "WiDaySunny" || icon.type.name === "WiDayOvercast" || icon.type.name === "WiDayCloudy") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#FFA500");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#FFD700");
-    }
-    else if (icon.type.name === "WiCloud") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#B0BEC5");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#78909C");
-    }
-    else if (icon.type.name === "WiRain") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#37474F");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#607D8B");
-    }
-    else if (icon.type.name === "WiThunderstorm") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#263238");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#546E7A");
-    }
-    else if (icon.type.name === "WiSnow") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#E1F5FE");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#B3E5FC");
-    }
-    else if (icon.type.name === "WiFog") {
-        document.documentElement.style.setProperty("--dynamic-background-1", "#ECEFF1");
-        document.documentElement.style.setProperty("--dynamic-background-2", "#B0BEC5");
+    switch (icon.type.name) {
+        case "WiDaySunny":
+        case "WiDayOvercast":
+        case "WiDayCloudy":
+            changeTheme("#FFA500", "#FFD700");
+            break;
+        case "WiCloud":
+            changeTheme("#B0BEC5", "#78909C", "#FFFFFF");
+            break;
+        case "WiRain":
+            changeTheme("#37474F", "#607D8B", "#FFFFFF");
+            break;
+        case "WiThunderstorm":
+            changeTheme("#263238", "#546E7A", "#FFFFFF");
+            break;
+        case "WiSnow":
+            changeTheme("#E1F5FE", "#B3E5FC");
+            break;
+        case "WiFog":
+            changeTheme("#ECEFF1", "#B0BEC5");
+            break;
     }
     
     return (
@@ -77,8 +81,8 @@ function Hero(props) {
             <Header getPlaceNameFromHeader={getPlaceNameFromHeader} getUnitsFromHeader={getUnitsFromHeader} locationName={props.locationName} date={props.date}></Header>
 
             <div className="hero-current-weather flex flex-col flex-center">
-                <span className="icon">{icon}</span>
-                <span className="current-temperature">{props.isMetric ? props.temperature + "°C" : ((props.temperature * 9/5) + 32).toFixed(1) + "F"}</span>
+                <span className="hero-icon">{icon}</span>
+                <span className="hero-current-temperature">{props.isMetric ? props.temperature + "°C" : ((props.temperature * 9/5) + 32).toFixed(1) + "F"}</span>
             </div>
         </div>
     );

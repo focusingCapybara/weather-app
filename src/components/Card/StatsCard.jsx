4ed29c693808cad9f00a019d1fce70bdd5a6ec33
Kacper Grabowski <kacperg390@gmail.com>
2025-03-12 17:17:14 +0000

    Changed StatsCard styles

--- src/components/Card/StatsCard.jsx
+++ src/components/Card/StatsCard.jsx
@@ -2,10 +2,12 @@ import styles from "./Card.module.css"
 
 function Card(props) {
     return (
-        <div className={`${styles.statsCard} ${styles.card} flex flex-col flex-center`}>
+        <div className={`${styles.statsCard} ${styles.card} flex flex-center`}>
             <h3>{props.title}</h3>
-            <div className="stats-icon">{props.icon}</div>
-            <span className={styles.valueItem}>{props.value}{props.unit}</span>
+            <div className="stats-info flex flex-center">
+                <div className={`${styles.valueItem} stats-icon`}>{props.icon}</div>
+                <span className={styles.valueItem}>{props.value}{props.unit}</span>
+            </div>
         </div>
     );
 }
